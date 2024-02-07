e5abccc9621b98f17da290c28eb684078a09fd7a
Ayman Parkar <ayman.parkar@gmail.com>
2024-02-07 21:00:59 +0530

    Update vuedraggable to version 4.1.0 and sortablejs to version 1.14.0

---
+++ src/components/KanbanColumn.vue
@@ -0,0 +1,65 @@
+<template>
+  <draggable
+    v-model="myArray"
+    v-bind="dragOptions"
+    style="height: 100%"
+    group="kanban"
+    @start="drag = true"
+    @end="drag = false"
+    item-key="id"
+  >
+    <template #item="{ element }">
+      <div style="height: 80px; border: 1px solid white; margin-top: 10px; margin-bottom: 10px">
+        {{ element.name }}
+      </div>
+    </template>
+  </draggable>
+</template>
+
+<script setup lang="ts">
+import draggable from 'vuedraggable'
+import { computed, ref } from 'vue'
+const drag = ref(false)
+
+const myArray = defineModel<any>()
+
+const dragOptions = computed(() => {
+  return {
+    animation: 200,
+    group: 'description',
+    disabled: false,
+    ghostClass: 'ghost'
+  }
+})
+</script>
+
+<style>
+.button {
+  margin-top: 35px;
+}
+
+.flip-list-move {
+  transition: transform 0.5s;
+}
+
+.no-move {
+  transition: transform 0s;
+}
+
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+
+.list-group {
+  min-height: 20px;
+}
+
+.list-group-item {
+  cursor: move;
+}
+
+.list-group-item i {
+  cursor: pointer;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,79 @@
 <template>
-  <SideLayout class="text-gray-400 font-semibold text-sm h-screen"> my kontent </SideLayout>
+  <SideLayout class="h-screen">
+    <button @click="() => arrayGreen.push({ id: 20, name: 'Added item' })">
+      Add item in green
+    </button>
+    <div style="display: flex; flex-direction: row; gap: 20px">
+      <div style="width: 100%; padding: 10px; border: 1px solid white">
+        <KanbanColumn v-model="arrayGreen" />
+      </div>
+      <div style="width: 100%; padding: 10px; border: 1px solid white">
+        <KanbanColumn v-model="array2" />
+      </div>
+      <div style="width: 100%; padding: 10px; border: 1px solid white">
+        <KanbanColumn v-model="array3" />
+      </div>
+      <div style="width: 100%; padding: 10px; border: 1px solid white">
+        <KanbanColumn v-model="array4" />
+      </div>
+    </div>
+  </SideLayout>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
+import KanbanColumn from '@/components/KanbanColumn.vue'
 import SideLayout from '@/components/SideLayout.vue'
+import { ref } from 'vue'
+
+const arrayGreen = ref([
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3' },
+  { id: 4, name: 'Item 4' },
+  { id: 5, name: 'Item 5' },
+  { id: 6, name: 'Item 6' },
+  { id: 7, name: 'Item 7' },
+  { id: 8, name: 'Item 8' },
+  { id: 9, name: 'Item 9' },
+  { id: 10, name: 'Item 10' }
+])
+
+const array2 = ref([
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3' },
+  { id: 4, name: 'Item 4' },
+  { id: 5, name: 'Item 5' },
+  { id: 6, name: 'Item 6' },
+  { id: 7, name: 'Item 7' },
+  { id: 8, name: 'Item 8' },
+  { id: 9, name: 'Item 9' },
+  { id: 10, name: 'Item 10' }
+])
+
+const array3 = ref([
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3' },
+  { id: 4, name: 'Item 4' },
+  { id: 5, name: 'Item 5' },
+  { id: 6, name: 'Item 6' },
+  { id: 7, name: 'Item 7' },
+  { id: 8, name: 'Item 8' },
+  { id: 9, name: 'Item 9' },
+  { id: 10, name: 'Item 10' }
+])
+
+const array4 = ref([
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3' },
+  { id: 4, name: 'Item 4' },
+  { id: 5, name: 'Item 5' },
+  { id: 6, name: 'Item 6' },
+  { id: 7, name: 'Item 7' },
+  { id: 8, name: 'Item 8' },
+  { id: 9, name: 'Item 9' },
+  { id: 10, name: 'Item 10' }
+])
 </script>
