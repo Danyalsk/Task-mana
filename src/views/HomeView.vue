f10590f7d8c5601606d5a6bf54abc21b590d4d01
danyal sameer khalife <141676015+Danyalsk@users.noreply.github.com>
2024-02-07 18:45:52 +0530

    Add initial project files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <SideLayout class="text-gray-400 font-semibold text-sm h-screen"> my kontent </SideLayout>
+</template>
+
+<script lang="ts" setup>
+import SideLayout from '@/components/SideLayout.vue'
+</script>
