80cf0421129fc4fb571f0fcfef9290b17442273c
danyal sameer khalife <141676015+Danyalsk@users.noreply.github.com>
2024-09-05 04:41:05 +0530

    refactor: Update home route path to '/'

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
 // import SignUpView from '@/views/SignUpView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/home',
+      path: '/',
       name: 'home',
-      component: HomeView
+      component: import('@/views/HomeView.vue')
     },
     // {
     //   path: '/signup',
